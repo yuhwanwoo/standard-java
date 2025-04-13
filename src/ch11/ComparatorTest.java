5ff5cd95f38697e0290f59d2164c221e3f5f23b0
yuhwanwoo <6514687@naver.com>
2025-04-13 11:44:33 +0900

    comp test

---
+++ src/ch11/ComparatorTest.java
@@ -0,0 +1,24 @@
+package ch11;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ComparatorTest {
+    public static void main(String[] args) {
+        String[] strArr = {"cat", "Dog", "lion", "tiger"};
+        Arrays.sort(strArr, new Descending());
+    }
+
+
+}
+class Descending implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        if (o1 instanceof Comparable && o2 instanceof Comparable) {
+            Comparable c1 = (Comparable) o1;
+            Comparable c2 = (Comparable) o2;
+            return c1.compareTo(c2) * -1;
+        }
+        return -1;
+    }
+}
